76d2d7b96a0ef7ffe7f6349a24bd0c8bc54831fb
InnaUsenko <innausenko8@gmail.com>
2023-08-01 15:47:07 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const myBody = document.querySelector("body");
+const myButton = document.querySelector("button.change-color");
+const mySpan = document.querySelector("span.color");
+
+myButton.addEventListener("click", (event) => {
+  let bgColor = getRandomHexColor();
+  myBody.style.backgroundColor = bgColor;
+  mySpan.textContent = bgColor;
+});
