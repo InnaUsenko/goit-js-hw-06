8fd4d5dbea9b9c45c230b5bca655acaabbb0c14c
InnaUsenko <innausenko8@gmail.com>
2023-08-01 10:41:12 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -8,8 +8,10 @@ const ingredients = [
 ];
 
 const mainElement = document.querySelector("#ingredients");
+const childrens = [];
 ingredients.forEach(elem => {
   const curItem = document.createElement("li");
   curItem.textContent = elem;
-  mainElement.append(curItem);
+  childrens.push(curItem);
 });
+mainElement.append(...childrens);
