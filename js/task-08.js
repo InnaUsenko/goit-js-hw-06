7f068a137a1b6266ba2b6f6cb931112683088c88
InnaUsenko <innausenko8@gmail.com>
2023-08-01 15:10:51 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,15 @@
+const myForm = document.querySelector("form.login-form");
 
+myForm.addEventListener("submit", myFun);
+
+function myFun(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+  if (email.value === "" || password.value === "") {
+    alert("All fields must be filled!");
+  }
+  console.log(`Email: ${email.value}, Password: ${password.value}`);
+  event.currentTarget.reset();
+}
