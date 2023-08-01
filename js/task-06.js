45688948b3cb59f3cb2f439a348251731040d323
InnaUsenko <innausenko8@gmail.com>
2023-08-01 14:37:20 +0300

    Update task-06.js

---
+++ js/task-06.js
@@ -0,0 +1,12 @@
+const inputEl = document.querySelector("#validation-input");
+
+inputEl.addEventListener("blur", (event) => {
+  let outputText = event.currentTarget.value;
+  inputEl.classList.remove("invalid");
+  inputEl.classList.remove("valid");
+  if (outputText.length < inputEl.dataset.length) {
+    inputEl.classList.toggle("invalid");
+  } else {
+    inputEl.classList.toggle("valid");
+  }
+});
